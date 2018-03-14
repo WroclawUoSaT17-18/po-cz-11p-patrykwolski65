a267670cec5681a5cae7d5e62fe46b5476ff0fa4
patrykwolski65 <36730007+patrykwolski65@users.noreply.github.com>
2018-03-14 15:10:22 +0100

    Zadanie domowe

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Program
+{
+    class oceny
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("podaj 3 oceny i zatwierdz kazda z nich");
+            Console.WriteLine("mechanika:");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("analiza matematyczna:");
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine("systemy wytwarzania i montazu:");
+            double z = double.Parse(Console.ReadLine());
+            student xxx  = new student(x, y, z);
+            Console.WriteLine(xxx.sredniaocen());
+            Console.ReadKey();
+        }
+    }
+}
+class student
+{
+    public double a;
+    public double b;
+    public double c;
+
+    public student(double ocena, double ocena2, double ocena3)
+    {
+        a = ocena;
+        b = ocena2;
+        c = ocena3;
+    }
+    public double sredniaocen()
+    {
+        Console.WriteLine("średnia wszystkich ocen wynosi:");
+        return ((a + b + c) / 3);
+    }
+}
